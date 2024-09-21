f2b09dc4307d8aa1e8734b09a920b6a4ddd46572
Karthik Baswaraju <karthikbaswaraju3@gmail.com>
2024-09-21 20:58:32 +0530

    first commit

---
+++ src/main/java/com/karthik/beans/ReportService.java
@@ -0,0 +1,13 @@
+package com.karthik.beans;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReportService {
+	
+public ReportService() {
+	
+	System.out.println("Report Service: Constructor ");
+}
+
+}
---
+++ src/main/java/com/karthik/dao/ReportDao.java
@@ -0,0 +1,10 @@
+package com.karthik.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ReportDao {
+public ReportDao() {
+System.out.println("Report DAO Constructor ");
+}
+}
